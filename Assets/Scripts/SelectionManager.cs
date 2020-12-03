3c5480baa0711fc56c4f6f6638c02ed4f77666fc
nshelly <dannyjos@yandex.ru>
2020-12-03 16:30:21 +0300

    Add button create, Change color when select

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material defaultMaterial;
+    
+    private Transform _selection;
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag((selectableTag)))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+            
+        }
+    }
+}
